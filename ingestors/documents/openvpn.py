90e319a71ed231e42a7d99c05489e99c9f7daebf
Nazar Vasylchuk <nazarnazar3000@gmail.com>
2024-02-19 19:44:46 +0200

    certificates parsing added

--- ingestors/documents/openvpn.py
+++ ingestors/documents/openvpn.py
@@ -28,56 +28,60 @@ class OpenVpnIngestor(Ingestor, EncodingSupport, TempFileSupport):
         text = self.read_file_decoded(entity, file_path)
         entity.set("bodyText", text)
 
-        parsed_config = self.parse_openvpn_config(file_path)
-        for key, value in parsed_config.items():
-            entity.add(key, value)
+        certificates_folder_entity = self.ingest_folder(entity, 'certificates')
 
-        self.ingest_cert(entity, "cert-test-1", "text/plain", "cert-body-1")
+        parsed_config = self.parse_openvpn_config(file_path)
+        properties = ''
+        for key, value in parsed_config.items():
+            if key == 'certificates':
+                for certificate in value:
+                    self.ingest_file(certificates_folder_entity, certificate['type'] + '.txt', "text/plain", certificate['content'])
+            else:
+                entity.add(key, value)
+                properties += f"{key}: {value}\n"
+
+        if properties != '':
+            self.ingest_file(entity, 'properties.txt', "text/plain", properties)
 
     def parse_openvpn_config(self, filename):
         config = {}
         with open(filename, 'r') as file:
+            current_cert_type = None
+            current_cert_content = ""
             for line in file:
-                # Extract server domain name
+                # Extract server domain name and port
                 match = re.match(r'^remote\s+([\w.-]+)\s+(\d+)', line)
                 if match:
                     config['serverDomain'] = match.group(1)
                     config['serverPort'] = match.group(2)
                 
-                # Extract network address
+                # Extract network address and subnet mask
                 match = re.match(r'^server\s+([\d.]+)\s+([\d.]+)', line)
                 if match:
                     config['networkAddress'] = match.group(1)
                     config['subnetMask'] = match.group(2)
                 
-                # Extract certificates
-                # match = re.match(r'^(ca|cert|key)\s+(.*)', line)
-                # if match:
-                #     cert_type = match.group(1)
-                #     cert_path = match.group(2)
-                #     config[cert_type + '_file'] = cert_path.strip()
-                #     with open(cert_path.strip(), 'r') as cert_file:
-                #         config[cert_type + '_content'] = cert_file.read()
+                # Extract certificates, keys, and passwords (tls-auth)
+                match = re.match(r'^(<ca>|<cert>|<key>|<tls-auth>)(?:\s+|$)(.*)', line)
+                if match:
+                    cert_type = match.group(1)[1:-1]
+                    current_cert_type = cert_type
+                    current_cert_content = ""
                 
-                # Extract passwords (tls-auth)
-                # match = re.match(r'^tls-auth\s+(.*)\s+(\d+)', line)
-                # if match:
-                #     config['tls_auth_file'] = match.group(1)
-                #     with open(match.group(1), 'r') as auth_file:
-                #         config['tls_auth_content'] = auth_file.read()
+                # If current certificate type is set, append the line to its content
+                if current_cert_type:
+                    current_cert_content += line
+                    if line.strip() == '</' + current_cert_type + '>':
+                        # This line indicates end of certificate/key block
+                        if 'certificates' not in config:
+                            config['certificates'] = []
+                        config['certificates'].append({'type': current_cert_type, 'content': current_cert_content})
+                        current_cert_type = None
         
         return config
     
-    def ingest_cert(self, entity, name, mime_type, body):
-        foreign_id = pathlib.PurePath(entity.id)
-        folder_name = "certificates"
-        foreign_id = foreign_id.joinpath(folder_name)
-        folder_entity = self.manager.make_entity("Folder", parent=entity)
-        folder_entity.add("fileName", folder_name)
-        folder_entity.make_id(foreign_id.as_posix())
-        self.manager.emit_entity(folder_entity)
-        
-        file_name = safe_filename(name, default="cert-1")
+    def ingest_file(self, entity, name, mime_type, body):
+        file_name = safe_filename(name, default="filename1")
         file_path = self.make_work_file(file_name)
         with open(file_path, "wb") as fh:
             if isinstance(body, str):
@@ -88,9 +92,18 @@ class OpenVpnIngestor(Ingestor, EncodingSupport, TempFileSupport):
         checksum = self.manager.store(file_path, mime_type=mime_type)
         file_path.unlink()
 
-        child = self.manager.make_entity("Document", parent=folder_entity)
+        child = self.manager.make_entity("Document", parent=entity)
         child.make_id(name, checksum)
         child.add("contentHash", checksum)
         child.add("fileName", name)
         child.add("mimeType", mime_type)
         self.manager.queue_entity(child)
+
+    def ingest_folder(self, entity, name):
+        foreign_id = pathlib.PurePath(entity.id)
+        foreign_id = foreign_id.joinpath(name)
+        folder_entity = self.manager.make_entity("Folder", parent=entity)
+        folder_entity.add("fileName", name)
+        folder_entity.make_id(foreign_id.as_posix())
+        self.manager.emit_entity(folder_entity)
+        return folder_entity
